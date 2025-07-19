76f04d1ac03984abd19bceb9c97d108b4e6717f3
DaoPM Dean <daopm.dean@gmail.com>
2025-07-19 16:09:17 +0700

    chapter 14: validate create form

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -27,6 +27,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceById = deleteInvoice.bind(null, id);
+  
   return (
     <form action={deleteInvoiceById} method="post">
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
