2ccfee7cdb90ef35b441ad736b152a28b2eeb159
DaoPM Dean <daopm.dean@gmail.com>
2025-07-19 16:54:26 +0700

    chapter 15: login & logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
@@ -112,4 +114,23 @@ export async function deleteInvoice(id: string) {
     console.error("Error deleting invoice:", error);
   }
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
